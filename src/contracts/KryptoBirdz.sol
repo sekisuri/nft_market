0146ecfab373847d3ac4da3c8f825a595dc3c2b4
sekisuri - GitHub <sekisuri@gmail.com>
2022-07-13 08:35:22 +0900

    setup

---
+++ src/contracts/KryptoBirdz.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Kryptobird {
+  string private name;
+  string private symbol;
+
+  constructor(){
+    name = 'Kryptobird';
+    symbol = 'KBIRDZ';
+  }
+}
