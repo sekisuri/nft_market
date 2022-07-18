6bcf26bb6b2c0232e2847eddb27105bda48d708d
sekisuri - GitHub <sekisuri@gmail.com>
2022-07-18 13:07:34 +0900

    nft 스마드계약02

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -5,7 +5,18 @@ import './ERC721Connector.sol';
 
 contract Kryptobird is ERC721Connector
 {
+  string[] public KryptoBirdz;
+
+  mapping(string => bool) _kryptoBirdzExists;
   
+  function mint(string memory _kryptoBird) public 
+  {
+    // this is deprecated uint _id = KryptoBirdz.push(_kryptoBird);
+    // .push no longer returns the length but a ref to the added element
+    KryptoBirdz.push(_kryptoBird);
+    uint _id = KryptoBirdz.length - 1;
+    _mint(msg.sender, _id);
+  }
   constructor() ERC721Connector('KryptoBird','KBIRD')
   {
     
