52caaf24aed132ddf436a27b5d38134263a76506
sekisuri - GitHub <sekisuri@gmail.com>
2022-07-18 13:10:00 +0900

    nft 02

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -3,14 +3,27 @@ pragma solidity ^0.8.0;
 
 contract ERC721
 {
+  event Transfer(
+    address indexed from,
+    address indexed to, 
+    uint256 indexed tokenId);
+
   mapping(uint => address) private _tokenOwner;
 
   mapping(address => uint256) private _OwnedTokensCount;
 
+  function _exists(uint256 tokenId) internal view returns(bool){
+    address owner = _tokenOwner[tokenId];
+    return owner != address(0);
+  }
+
   function _mint(address to,uint256 tokenId) internal
   {
-    require( to != address(0));
+    require( to != address(0),'ERC721: minting to the zero addres');
+    require(!_exists(tokenId),'ERC721: token already mint');
     _tokenOwner[tokenId] = to;
     _OwnedTokensCount[to] +=  1;
+
+    emit Transfer(address(0), to, tokenId);
   }
 }
